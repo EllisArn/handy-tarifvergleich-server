756928d179f78a5aceaf83702cb9baaf55dbe3b7
Ellis Arn <ellisarn@gmail.com>
2024-01-08 21:02:01 +0100

    CORS update

--- handy-tarifvergleich-server/Program.cs
+++ handy-tarifvergleich-server/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowOrigin",
-        corsPolicyBuilder => corsPolicyBuilder.WithOrigins("http://127.0.0.1:5500", "https://mobiltarifvergleich.netlify.app/")
+        corsPolicyBuilder => corsPolicyBuilder.WithOrigins("http://127.0.0.1:5500", "https://mobiltarifvergleich.netlify.app")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
